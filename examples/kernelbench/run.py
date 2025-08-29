62e6d96e3bcdb0b37d2ffff8d5c2babef842e9c4
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-08-29 09:30:41 -0700

    make async main function in kernelbench run script

--- examples/kernelbench/run.py
+++ examples/kernelbench/run.py
@@ -1,17 +1,22 @@
 import os
+import asyncio
 from pathlib import Path
 from openevolve import OpenEvolve
 
 curr_dir = Path(os.path.realpath(os.path.dirname(__file__)))
 
-# TODO: when we grab a task to run from KernelBench, must wrap it with EVOLVE blocks!
-init_program = curr_dir / "initial_program.py"
-# TODO: must have the evaluator read from a file to determine which task number we are dealing with
-eval_file = curr_dir / "evaluator.py"
-config_path = curr_dir / "config.yaml"
-# create a new run directory (based on current yyyy-mm-dd-... and place each individual task run in here)
-output_dir = curr_dir / "openevolve_output_level_0_task_5_trial_2"
+async def main():
+    # TODO: when we grab a task to run from KernelBench, must wrap it with EVOLVE blocks!
+    init_program = curr_dir / "initial_program.py"
+    # TODO: must have the evaluator read from a file to determine which task number we are dealing with
+    eval_file = curr_dir / "evaluator.py"
+    config_path = curr_dir / "config.yaml"
+    # create a new run directory (based on current yyyy-mm-dd-... and place each individual task run in here)
+    output_dir = curr_dir / "openevolve_output_level_0_task_5_trial_2"
 
-runner = OpenEvolve(init_program, eval_file, config_path=config_path, output_dir=output_dir)
+    runner = OpenEvolve(init_program, eval_file, config_path=config_path, output_dir=output_dir)
 
-runner.run()
+    await runner.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
